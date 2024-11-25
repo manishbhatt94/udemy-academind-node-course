4bf88a46d0be07a64ffb645b4e5e3703ec13a903
Manish Bhatt <manishbhatt594@gmail.com>
2024-11-25 12:42:27 +0530

    Instantiate sequelize instances

--- util/database.js
+++ util/database.js
@@ -1,19 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-// Create the connection pool. The pool-specific settings are the defaults
-const pool = mysql.createPool({
+const sequelize = new Sequelize('udemytest_database_1', 'dummyduck', 'dummyduck', {
   host: 'localhost',
-  user: 'dummyduck',
-  password: 'dummyduck',
-  database: 'udemytest_database_1',
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-  // idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
-  // queueLimit: 0,
-  // enableKeepAlive: true,
-  // keepAliveInitialDelay: 0,
+  dialect: 'mysql',
 });
-// dummyduck@172.22.114.121:3306
 
-module.exports = pool.promise();
+module.exports = sequelize;
