8e2d0d3fba272306b44e3f5d67903fe70b12361c
Manish Bhatt <manishbhatt594@gmail.com>
2024-12-04 14:42:36 +0530

    Switch to debug database when debugging

--- util/database.js
+++ util/database.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('udemytest_database_1', 'dummyduck', 'dummyduck', {
+const DB_NAME = process.env.NODE_ENV === 'debug' ? 'debug_session_db_01' : 'udemytest_database_1';
+
+const sequelize = new Sequelize(DB_NAME, 'dummyduck', 'dummyduck', {
   host: 'localhost',
   dialect: 'mysql',
 });
