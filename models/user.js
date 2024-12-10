a843e2899ae8c0c2ed987fc84d1da5aad7c88834
Manish Bhatt <manishbhatt594@gmail.com>
2024-12-10 16:16:56 +0530

    Store cart with user in mongo

--- models/user.js
+++ models/user.js
@@ -2,9 +2,10 @@ const { ObjectId } = require('mongodb');
 const { getDatabase } = require('../util/database');
 
 class User {
-  constructor({ _id, name, email }) {
+  constructor({ _id, name, email, cart }) {
     this.name = name;
     this.email = email;
+    this.cart = cart; // { items: [{productId, quantity}] }
     if (_id) {
       this._id = typeof _id === 'string' ? ObjectId.createFromHexString(_id) : _id;
     } else {
