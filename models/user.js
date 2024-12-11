87ac9b0b3197918a15e8a28874c67b34ee0cc57f
Manish Bhatt <manishbhatt594@gmail.com>
2024-12-11 17:59:58 +0530

    Switch to mongoose for add to cart functionality

--- models/user.js
+++ models/user.js
@@ -23,4 +23,26 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.methods.addToCart = function addToCart(product) {
+  const { _id: productId } = product;
+  const { items: cartItems } = this.cart;
+  let updatedCartItems = [...cartItems];
+  let newProductQuantity = 1;
+  const existingItemIndex = cartItems.findIndex((item) => {
+    return productId.toString() === item.product.toString(); // item.product contains product ID only
+  });
+  if (existingItemIndex > -1) {
+    // product already exists in cart - need to increment quantity
+    newProductQuantity = cartItems[existingItemIndex].quantity + 1;
+    updatedCartItems[existingItemIndex].quantity = newProductQuantity;
+  } else {
+    // product doesn't already exist in cart
+    updatedCartItems.push({ product: productId, quantity: newProductQuantity });
+  }
+  const updatedCart = { items: updatedCartItems };
+  this.cart = updatedCart;
+  return this.save();
+};
+
+
 module.exports = mongoose.model('User', userSchema);
