68d3559f286ad0b9b2c92b6d8c7521ed0c2b8b9f
Manish Bhatt <manishbhatt594@gmail.com>
2024-12-13 12:55:35 +0530

    Add auth routes and dummy auth controllers

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 
 const User = require('./models/user');
@@ -47,6 +48,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
